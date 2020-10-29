b85297c6b1fbe6ec3dd446fccf658531cea5eda2
delayedaa <delayedaa@gmail.com>
2020-10-29 08:54:57 -0500

    [app.js] Fix the stop button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 let input = document.querySelector('input');
 let startButton = document.querySelector('.start');
 let stopButton = document.querySelector('.stop');
-let slideshow = document.querySelector('.slideshow');
+let container = document.querySelector('.container');
 
 let numImages = 5;
 
 // Functions
 
 let runSlideshow = (imageUrls => {
+  let slideshow = document.querySelector('.slideshow');
   let i = 1;
   setInterval(() => {
     slideshow.style.backgroundImage = `url(${imageUrls[i]})`;
@@ -43,8 +44,10 @@ let handleClickStart = () => {
 
     // Post the first image, then unhide the slideshow div
 
+    let slideshow = document.createElement('div');
+    slideshow.classList.add('slideshow');
     slideshow.style.backgroundImage = `url(${imageUrls[0]})`;
-    slideshow.classList.remove('hidden');
+    container.appendChild(slideshow);
 
     // Run the "runSlideshow" function, and give it our new array of images as an argument
 
@@ -58,7 +61,8 @@ let handleClickStart = () => {
 }
 
 let = handleClickStop = () => {
-  slideshow.classList.add('hidden');
+  let slideshow = document.querySelector('.slideshow');
+  container.removeChild(slideshow);
   input.value = '';
 };
 
@@ -67,134 +71,3 @@ let = handleClickStop = () => {
 
 startButton.addEventListener('click', handleClickStart);
 stopButton.addEventListener('click', handleClickStop);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Version 1
-
-
-// let input = document.querySelector('input');
-// let startButton = document.querySelector('.start');
-// let stopButton = document.querySelector('.stop')
-// let slideshow = document.querySelector('.slideshow');
-// let allImages = document.querySelectorAll('img');
-
-// startButton.addEventListener('click', handleClickStart);
-// stopButton.addEventListener('click', handleClickStop)
-
-// function handleClickStart() {
-
-//   let searchTerm = input.value;
-
-
-
-//   fetch('https://www.reddit.com/search.json?nsfw=no&q=' + searchTerm)
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     debugger;
-//     let totalImages = 0;
-//     for (let i = 0; i < data.data.children.length; i++) {
-//       let url = data.data.children[i].data.url;
-//       if (url.endsWith('jpg') === true || url.endsWith('png') === true) {
-//         let image = allImages[i];
-//         image.src = data.data.children[i].data.url;
-//         totalImages++;
-//       }
-//       if (totalImages >= 6) {
-//         break;
-//       }
-//     }
-    
-    
-    
-    
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-
-// }
-
-// function handleClickStop() {
-//  for (let i = 0; i < allImages.length; i++) {
-//    slideshow.removeChild(allImages[i]);
-//  }
-//  input.value = '';
-// }
-
-
-
-
-
-    // let leftButton = document.querySelector('.left');
-    // let rightButton = document.querySelector('.right');
-    // let firstImage = document.querySelector('img');
-    // firstImage.classList.add('front-image');
-
-    // leftButton.addEventListener('click', function() {
-    //   // debugger;
-    //   let frontImage = document.querySelector('.front-image');
-    //   let frontImageId = frontImage.id;
-    //   if (frontImageId === '0') {
-    //     frontImageId = 5;
-    //   } else {
-    //     frontImageId--;
-    //   }
-    //   frontImage.classList.remove('front-image');
-    //   let newFrontImage = document.querySelector(`#${frontImageId}`);
-    //   newFrontImage.classList.add('front-image');
-    // })
-    
-    
-
-
-
-
-    
