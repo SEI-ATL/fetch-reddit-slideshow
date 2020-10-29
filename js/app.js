4c0de4110c269416cf5c9824665733fa081d35eb
Nitish Dayal <contact@nitishdayal.me>
2020-10-29 05:23:34 -0700

    feat: base functionality done, lacks styling

--- js/app.js
+++ js/app.js
@@ -1,31 +1,96 @@
-// Element selectors
+// ================================================================ ELEMENT SELECTORS
+const $container = document.querySelector('.container')
 const $title = document.querySelector('.title')
 const $description = document.querySelector('.description')
 const $searchInput = document.querySelector('input[name="search-term"]')
 const $submitButton = document.querySelector('.submit')
+const $loadingMsg = document.querySelector('.loading')
+const $carouselInner = document.querySelector('.carousel-inner')
 
 
-// Event Listeners
-$searchInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
+// ================================================================ HELPER FUNCTIONS
+/*
+  Create the following, where img src is from the API call:
+  <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+  </div>
+*/
+const createCarouselItems = (urls, searchTerm) => urls.map((url, index) => {
+  const $carouselDiv = document.createElement('div')
+  $carouselDiv.classList.add('carousel-item')
 
-    // console.log($searchInput.value)
-    $title.classList.add('hidden')
-    $description.classList.add('hidden')
-    $searchInput.classList.add('hidden')
-    $submitButton.classList.add('hidden')
+  if (index === 0) $carouselDiv.classList.add('active')
 
-    const searchValue = $searchInput.value
+  const $carouselImg = document.createElement('img')
+  $carouselImg.src = url
+  $carouselImg.alt = searchTerm
+  $carouselImg.classList.add('d-block', 'w-100')
 
-    fetch(`http://reddit.com/search.json?nsfw=no&q=${searchValue}`)
-      .then((response) => response.json())
-      .then(response => {
-        const data = response.data.children
+  $carouselDiv.appendChild($carouselImg)
 
-        const arrayOfImages = data
-          .map(item => item.data.url)
-          .filter(url => url.includes('.jpg'))
-      })
-      .catch(e => console.error(e))
-  }
+  return $carouselDiv
 })
+
+
+// Removes all children of a parent node
+const removeChildren = (parent) => {
+  while (parent.lastChild) parent.removeChild(parent.lastChild);
+}
+
+
+// ================================================================ EVENT HANDLERS
+const handleSearch = () => {
+  const searchValue = $searchInput.value
+
+  $loadingMsg.classList.remove('hidden')
+
+  fetch(`http://reddit.com/search.json?nsfw=no&q=${searchValue}`)
+    .then((response) => response.json())
+    .then(response => {
+      // Get array of 'children', the actual data returned from the API
+      const data = response.data.children;
+
+      // Hide all the elements
+      [$title, $description, $searchInput, $submitButton, $loadingMsg].forEach(el => {
+        el.classList.add('hidden')
+      })
+
+      // Go through the data from the API, get all the URLs, and filter out anything that isn't
+      // a JPG
+      const arrayOfImages = data
+        .map(item => item.data.url)
+        .filter(url => url.includes('.jpg'))
+
+      // Create carousel items out of the JPGs from the API call
+      const arrayOfCarouselItems = createCarouselItems(arrayOfImages, searchValue)
+
+      // Add each of the carousel items to the carousel component in the DOM
+      arrayOfCarouselItems.forEach(item => {
+        $carouselInner.appendChild(item)
+      })
+
+      // Create a 'Stop' button to stop the carousel
+      const $stopButton = document.createElement('button')
+      $stopButton.classList.add('btn', 'btn-primary')
+      $stopButton.textContent = 'Stop'
+
+      // When 'stop' is clicked, delete carousel items and show search again
+      $stopButton.addEventListener('click', (e) => {
+        removeChildren($carouselInner);
+
+        [$title, $description, $searchInput, $submitButton].forEach(el => {
+          el.classList.remove('hidden')
+        })
+
+        $container.removeChild($stopButton)
+      })
+
+      $container.appendChild($stopButton)
+    })
+    .catch(e => console.error(e))
+}
+
+
+// ================================================================ EVENT LISTENERS
+$searchInput.addEventListener('keypress', (e) => { if (e.key === 'Enter') handleSearch() })
+$submitButton.addEventListener('click', handleSearch)
