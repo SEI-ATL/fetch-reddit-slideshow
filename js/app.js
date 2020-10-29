7c4baf70864f28c25545abed609ee3145078b6d8
Will Locklier <will.locklier@gmail.com>
2020-10-28 21:04:45 -0400

    [slideshow] Add initial files

---
+++ js/app.js
@@ -0,0 +1,44 @@
+document.getElementById('submit-button').addEventListener('click', function() {
+    const search = document.getElementById('search-input').value
+    setInterval(() => fetchImage(search), 5000);
+
+})
+document.getElementById('reset-button').addEventListener('click', function() {
+    clearInterval(5000)
+})
+
+function fetchImage(search) {
+    fetch (`https://www.reddit.com/search.json?q=${search}+nsfw:no`)
+    .then(redditData => redditData.json())
+    .then((json) => {
+        const randomPost = getRandomPost(json.data.children)
+        const image = randomPost.data.thumbnail;
+        console.log("hello", image)
+
+        appendImageToDom(image);
+
+    })
+
+    function getRandomPost(posts) {
+        const randomIndex = Math.floor(Math.random() * posts.length)
+        return posts[randomIndex]
+    }
+}
+// Add picture to DOM
+// Update picture every 5 seconds
+// cancel button
+
+function appendImageToDom(image) {
+    document.getElementById('reddit-image').setAttribute('src', image);
+
+
+    // randomImage.src = image;
+    // document.querySelector('#display-search').append(randomImage);
+}
+
+// set interval id somewhere
+
+var interval = setInterval
+
+
+// document.getElementById('reddit-image').setAttribute('src', image);
