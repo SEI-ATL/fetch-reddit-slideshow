d561283be933b2d9f1081baf85fa6fd9b61dca75
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-29 07:06:42 -0700

    [js] add working button now and got setInterval to work. need to work on the image replacing itself

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@
 
 document.getElementById('submit-button').addEventListener('click', function (){
     let query = document.getElementById('query-input').value 
-    fetchNow(query);
+    setInterval(fetchNow, 5000, query);
 })
 document.getElementById('reset-button').addEventListener('click', function (){
-  let remove = document.querySelectorAll("#images").setAttribute('src', " ")
-  return remove
+  let images = document.querySelectorAll("#images")
+  for (let i=0; i < images.length; i++) {
+      let eachImage = images[i]
+      eachImage.setAttribute('src', "")
+  }
+  
     
 })
 function appendPicToDom(url) {
@@ -15,6 +19,7 @@ function appendPicToDom(url) {
     image.id = "images";
     document.querySelector(".container").appendChild(image)
 }
+
 
 
 let fetchNow = function(query)
