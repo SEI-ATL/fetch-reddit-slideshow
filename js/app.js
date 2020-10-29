908332f039c4e51192ddd44e189df5fc3c7c4430
Will Locklier <will.locklier@gmail.com>
2020-10-29 10:01:21 -0400

    [app] Update file to add features

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-document.getElementById('submit-button').addEventListener('click', function() {
+    document.getElementById('submit-button').addEventListener('click', function() {
     const search = document.getElementById('search-input').value
     var interval = setInterval(() => fetchImage(search), 5000);
+    let button = document.querySelector('#submit-button')
+    let title = document.querySelector('.title')
+    let inputBox = document.querySelector('#search-input')
+    button.style.visibility = 'hidden'
+    title.style.visibility = 'hidden'
+    inputBox.style.visibility = 'hidden'
+
+
+    
     document.getElementById('reset-button').addEventListener('click', function() {
         clearInterval(interval)
+    button.style.visibility = 'visible'
+    title.style.visibility = 'visible'
+    inputBox.style.visibility = 'visible'
 
+    
 })
-
 })
 
 function fetchImage(search) {
@@ -39,7 +51,6 @@ function appendImageToDom(image) {
 
 // set interval id somewhere
 
-var interval = setInterval
 
 
 // document.getElementById('reddit-image').setAttribute('src', image);
